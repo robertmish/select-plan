07ae85e9559f0462ad7913e258ee5945782de1db
robertmish <robertmish@gmail.com>
2023-12-23 19:57:35 +0100

    plans selection added

--- script.js
+++ script.js
@@ -12,3 +12,36 @@ $(document).ready(function () {
     $("#page-mask").css("display", "none");
   });
 });
+
+$(document).ready(function () {
+  $("#basic").click(function (e) {
+    $("#basic").css("border", "1px solid blue");
+  });
+});
+
+$(document).ready(function () {
+  $("#basic").click(function (e) {
+    $("#selected_plan").val("Basic");
+    $("#basic").css("border", "1px solid blue");
+    $("#standard").css("border", "1px solid #dedede");
+    $("#premium").css("border", "1px solid #dedede");
+  });
+});
+
+$(document).ready(function () {
+  $("#standard").click(function (e) {
+    $("#selected_plan").val("Standard");
+    $("#standard").css("border", "1px solid blue");
+    $("#basic").css("border", "1px solid #dedede");
+    $("#premium").css("border", "1px solid #dedede");
+  });
+});
+
+$(document).ready(function () {
+  $("#premium").click(function (e) {
+    $("#selected_plan").val("Premium");
+    $("#premium").css("border", "1px solid blue");
+    $("#standard").css("border", "1px solid #dedede");
+    $("#basic").css("border", "1px solid #dedede");
+  });
+});
